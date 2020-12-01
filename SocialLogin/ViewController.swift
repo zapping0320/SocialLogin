28519f92a3faf6bb11b80ef30c080fd45dc5549f
john donghyun kim <zapping0320@gmail.com>
2020-12-01 13:28:06 +0900

    Initial Commit

---
+++ SocialLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialLogin
+//
+//  Created by DD Dev on 2020/12/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
